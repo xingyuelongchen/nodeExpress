f0089159acef3a3458aa1acc44bff345204ea579
hello wen <xingyuelongchen@qq.comm>
2019-09-04 23:38:04 +0800

    update

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const config = require('./lib/config');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const router = require('./lib/router');
+const config = require('./lib/config');
 app.listen(config.server.port, config.server.domain, () => {
-    console.log('ok')
+    console.log('server ', config.server.domain, config.server.port)
+});
+// 挂载 post请求体解析模块
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'text/plain' }));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(express.static('src'));
+app.use('/favicon.ico', function () {
+    return
 })
-app.use(express.static('src'))
 app.use(router)
---
+++ src/index/index.js
@@ -0,0 +1,3 @@
+module.exports = function (req, res, config) {
+    res.send('index')
+}
